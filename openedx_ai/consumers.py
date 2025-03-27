995ce2de9d9a2cb11cf557ec73bc307c2fbe5dea
Kyrylo Kholodenko <kyrylo.kholodenko@raccoongang.com>
2025-03-27 18:32:45 +0200

    feat: fetch context about course

--- openedx_ai/consumers.py
+++ openedx_ai/consumers.py
@@ -1,28 +1,17 @@
+import requests
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 from openai import AsyncOpenAI
 
 
-class EchoConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await self.send(text_data="WebSocket connected!")
-
-    async def receive(self, text_data):
-        await self.send(text_data=text_data)
-
-    async def disconnect(self, close_code):
-        print("WebSocket disconnected:", close_code)
-
-
 class ChatGPTConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.course_id = self.scope["url_route"]["kwargs"]["course_id"]
-        # TODO: Provide openedx course context
+        data = requests.get(f"{settings.LMS_BASE_URL}/api/mentoring/ai/context/{self.course_id}").json()
         self.messages = [
             {
                 "role": "system",
-                "content": f"You are a helpful AI assistant for course {self.course_id}. Respond concisely and clearly.",
+                "content": f"You are a helpful AI assistant for a course. Your goal is to assist learners by providing clear and concise explanations, answering their questions, and guiding them through the course content. Focus only on the information provided in the course. Do not add extra content, imagine details, or provide information that is not explicitly included in the course material. Adapt your responses based on the learner's needs, ensuring clarity and engagement. Context:\n{data}",  # pylint: disable=line-too-long
             }
         ]
         await super().connect()
